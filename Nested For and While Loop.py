b1ee506d15358f9a25f1446e00071725e6e48d61
Mohit <118451175+Mohit26Bali@users.noreply.github.com>
2022-11-25 18:37:45 +0530

    Add files via upload
    some easy mcqs on loops

---
+++ Nested For and While Loop.py
@@ -0,0 +1,62 @@
+'''adj=['red','big','tasty']
+fruits=['apple','banana','cherry']
+for x in adj:
+    for y in fruits:
+        print(x,y)'''
+
+'''for i in range(2):
+    print(i)
+    for j in range(10,13):
+        print(j)'''
+
+#program_for_printing_tables:-
+
+
+'''for i in range(1,11):
+    for j in range(1,11):
+        print(i,"*",j,"=",i*j)
+    print()'''
+
+#Pattern_program:-
+
+''''n=int(input("Enter a range: "))
+for i in range(0,n):
+    for j in range(0,i+1):
+        print('*',end='')
+    print('\n')'''
+
+'''for i in range(0,5):
+    for j in range(0,i+1):
+        print('*',end='')
+    print('\n')'''
+
+'''for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print("\n")'''
+
+#NESTED_WHILE_LOOP:-
+
+'''i=1
+while i<=3:
+    print(i,"Outer loop is exexcuted only once")
+    j=1
+    while j<=3:
+        print(j,"inner loop is executed till completion")
+        j+=1
+    i+=1;'''
+
+
+#infinite_while_loop:-
+
+
+'''while(True):
+    print('Statements outside loop in printed is only once.')
+    while(True):
+        print('Statements of inner loop is printed till it is completed.')'''
+
+
+
+
+
+
